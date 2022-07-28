65c8307049a18bec62ee34a0d4a90d84a11fd293
krhuber <kr.huber@gmail.com>
2022-07-28 15:55:34 -0400

    Showing the current Temperature
    Started JS file with API data for current temperature

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "64f18b4413b6b44d53aaeab0328413a9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
